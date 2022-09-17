dae4dad60635f5fdce2341416b23091fb4727270
Luo Yi <yilozt@outlook.com>
2022-09-17 12:38:53 +0800

    use mirror from sjtu

--- system/base.nix
+++ system/base.nix
@@ -5,8 +5,9 @@
 
   # mirror
   nix.settings.substituters = [
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
+    # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    # "https://mirrors.ustc.edu.cn/nix-channels/store"
   ];
 
   # hostname.
